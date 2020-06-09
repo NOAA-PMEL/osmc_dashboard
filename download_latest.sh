0742fcc02c2bbf0536cc763aa101249f3d9bb633
noaaroland <roland.schweitzer@noaa.gov>
2020-06-09 11:15:05 -0700

    Fix URL details.

--- download_latest.sh
+++ download_latest.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-curl 'http://osmc.noaa.gov/erddap/tabledap/OSMC_30day.csv?platform_code%2Cplatform_type%2Ctime%2Clatitude%2Clongitude%2Cobservation_depth%2Cztmp%2Czsal%2Csst%2Catmp%2Cslp%2Cwindspd%2Cwinddir%2Cclouds
-%2Cdewpoint&time%3E=now-14days&latitude<=90&longitude<=180&platform_type!="WEATHER OBS"&platform_type!="WEATHER AND OCEAN OBS"&platform_type!="VOLUNTEER OBSERVING SHIPS"&platform_type!="RESEARCH"' -o refresh.csv
+curl 'http://osmc.noaa.gov/erddap/tabledap/OSMC_30day.csv?platform_code%2Cplatform_type%2Ctime%2Clatitude%2Clongitude%2Cobservation_depth%2Cztmp%2Czsal%2Csst%2Catmp%2Cslp%2Cwindspd%2Cwinddir%2Cclouds%2Cdewpoint&time%3E=now-14days&latitude%3C=90&longitude%3C=180&platform_type!=%22WEATHER%20OBS%22&platform_type!=%22WEATHER%20AND%20OCEAN%20OBS%22&platform_type!=%22VOLUNTEER%20OBSERVING%20SHIPS%22&platform_type!=%22RESEARCH%22' -o refresh.csv
 if [ $? -eq 0 ]
 then
   mv refresh.csv latest.csv
